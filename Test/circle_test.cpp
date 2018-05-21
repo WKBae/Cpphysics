69dbe91fba5eac674d7fb97192a06cc17f4c20d0
WKBae <williambae1@gmail.com>
2018-05-21 22:30:25 +0900

    Join common circle creations in test

--- Test/circle_test.cpp
+++ Test/circle_test.cpp
@@ -7,33 +7,35 @@ using namespace Microsoft::VisualStudio::CppUnitTestFramework;
 TEST_CLASS(CircleTest)
 {
 public:
-		
+	Circle* circle;
+	TEST_METHOD_INITIALIZE(initCircle) {
+		circle = new Circle(10);
+	}
+	TEST_METHOD_CLEANUP(cleanCircle) {
+		delete circle;
+	}
+
 	TEST_METHOD(testCreation) {
-		Circle* circle = new Circle(10);
 		Assert::AreEqual(circle->radius, 10);
 	}
 
 	TEST_METHOD(testMass) {
-		Circle* circle = new Circle(10);
 		circle->setMass(5);
 		Assert::AreEqual(circle->mass, 5);
 	}
 
 	TEST_METHOD(testInitialPosition) {
-		Circle* circle = new Circle(10);
 		Assert::AreEqual(circle->x, 0);
 		Assert::AreEqual(circle->y, 0);
 	}
 
 	TEST_METHOD(testSetPosition) {
-		Circle* circle = new Circle(10);
 		circle->setPosition(2, 3);
 		Assert::AreEqual(circle->x, 2);
 		Assert::AreEqual(circle->y, 3);
 	}
 
 	TEST_METHOD(testVelocity) {
-		Circle* circle = new Circle(10);
 		circle->setVelocity(5, 8);
 		Assert::AreEqual(circle->velocityX, 5);
 		Assert::AreEqual(circle->velocityY, 8);
